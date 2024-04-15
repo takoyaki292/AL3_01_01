702e962060c8941a755ed46cd6b1a010b1dba850
takoyaki292 <k023g1046@g.neec.ac.jp>
2024-04-15 17:36:03 +0900

    名前を入れた

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -20,7 +20,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ゲームウィンドウの作成
 	win = WinApp::GetInstance();
-	win->CreateGameWindow();
+	win->CreateGameWindow("GC2B_08_シミズ_タクミ");
 
 	// DirectX初期化処理
 	dxCommon = DirectXCommon::GetInstance();
