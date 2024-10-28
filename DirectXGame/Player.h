50c6c042c6d3d1521dce973274499818cb251e58
takoyaki292 <k023g1046@g.neec.ac.jp>
2024-10-28 15:07:43 +0900

    q

--- DirectXGame/Player.h
+++ DirectXGame/Player.h
@@ -155,7 +155,7 @@ private:
 
 	bool onGround_ = true;
 
-	static inline const float kGravityAcceleration = 0.05f;
+	//static inline const float kGravityAcceleration = 0.05f;
 	static inline const float kLimitFallSpeed = 0.5f;
 	static inline const float kJumpAcceleration = 0.7f;
 	
