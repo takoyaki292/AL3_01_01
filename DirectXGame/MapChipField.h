3b6292f7373b5c31d11198e2988de3c4f4d36dfa
takoyaki292 <k023g1046@g.neec.ac.jp>
2024-10-28 15:58:34 +0900

    a

--- DirectXGame/MapChipField.h
+++ DirectXGame/MapChipField.h
@@ -30,8 +30,8 @@ public:
 
 	Vector3 GetMapChipPositionByIndex(uint32_t xIndex, uint32_t yIndex);
 	private:
-	static inline const float kBlockWidth = 1.0f;
-	static inline const float kBlockHeight = 1.0f;
+	static inline const float kBlockWidth = 2.0f;
+	static inline const float kBlockHeight = 2.0f;
 
 	static inline const uint32_t kNumBlockVirtical = 20;
 	static inline const uint32_t kNumBlockHorizontal = 100;
