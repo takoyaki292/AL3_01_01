5e6c293813f93e79323c1f136f78166222ba64fa
takoyaki292 <k023g1046@g.neec.ac.jp>
2024-11-14 10:50:15 +0900

    a

--- DirectXGame/scene/GameScene.cpp
+++ DirectXGame/scene/GameScene.cpp
@@ -48,28 +48,15 @@ void GameScene::Initialize() {
 	{
 		worldTransformBlocks_[i].resize(kNumBlockHorizontal);
 	}
-	//拡張for文
-	//
-	//ここでキューブを作成する
-	//for (uint32_t i = 0; i < kNumBlockHorizontal; ++i)
-	//{
-	//	worldTransformBlocks_[i]=new WorldTransform;
-	//	worldTransformBlocks_[i]->Initialize();
-	//	worldTransformBlocks_[i]->translation_.x = kBlockWidth * i;
-	//	worldTransformBlocks_[i]->translation_.y = 0.0f;
-	//}
+	
 	for (uint32_t i = 0; i < kNumBlockVirtical; ++i)
 	{
 		for (uint32_t j = 0; j < kNumBlockHorizontal; ++j)
 		{
-			if (i%2==0&&j%2==0)
-			{
-				worldTransformBlocks_[i][j] = new WorldTransform();
-				worldTransformBlocks_[i][j]->Initialize();
-				worldTransformBlocks_[i][j]->translation_.x = kBlockWidth *j;
-				worldTransformBlocks_[i][j]->translation_.y = kBlockHeight * i;
-
-			}
+			worldTransformBlocks_[i][j] = new WorldTransform();
+			worldTransformBlocks_[i][j]->Initialize();
+			worldTransformBlocks_[i][j]->translation_.x = kBlockWidth * j;
+			worldTransformBlocks_[i][j]->translation_.y = kBlockHeight * i;
 		}
 	}
 	viewProjection_.Initialize();
